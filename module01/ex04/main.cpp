aa5d523da0c76bdb053eaf19603f22b6fbabc308
SimoneFelici <felicisimone9@gmail.com>
2025-09-21 21:05:11 +0200

    fix

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -1,43 +1,55 @@
 #include <iostream>
 #include <string>
-#include <fstream> 
+#include <fstream>
 
-void replace(size_t pos, std::string &row, std::string s1, std::string s2)
+void replace(size_t pos, std::string &row, const std::string &s1, const std::string &s2)
 {
-    row = row.substr(0, pos) + s2 + row.substr(pos + s1.size(), row.size());
+    row = row.substr(0, pos) + s2 + row.substr(pos + s1.size());
 }
 
 int main(int argc, char **argv)
 {
-    std::string input;
-    size_t i = 0;
+    if (argc != 4)
+    {
+        std::cerr << "Wrong number of args.\nUsage: " << argv[0] << " filename s1 s2" << std::endl;
+        return 1;
+    }
 
-    if (argc == 4)
+    const std::string out = argv[1];
+    const std::string s1  = argv[2];
+    const std::string s2  = argv[3];
+
+    if (s1.empty())
     {
-        const std::string out = argv[1];
-        std::ifstream infile(argv[1]);
-        if (!infile)
-        {
-            std::cerr << "Cant open " << argv[1] << std::endl;
-            return 1;
-        }
-        std::ofstream outfile(out + ".replace");
-        if (!outfile)
-        {
-            std::cerr << "Cant create " << out << std::endl; return 1;
-        }
-        while (std::getline (infile, input))
-        {
-            i = input.find(argv[2], 0);
-            if (i != std::string::npos)
-                replace(i, input, argv[2], argv[3]);
-            outfile << input << std::endl;
-        }
-        infile.close();
-        outfile.close();
+        std::cerr << "s1 must not be empty" << std::endl;
+        return 1;
     }
-    else
+
+    std::ifstream infile(argv[1]);
+    if (!infile)
     {
-        std::cout << "Wrong number of args." << std::endl << "Usage: " << argv[0] << " filename s1 s2" << std::endl;
+        std::cerr << "Cant open " << argv[1] << std::endl;
+        return 1;
     }
+
+    std::ofstream outfile((out + ".replace").c_str());
+    if (!outfile)
+    {
+        std::cerr << "Cant create " << (out + ".replace") << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(infile, line))
+    {
+        size_t pos = 0;
+        while ((pos = line.find(s1, pos)) != std::string::npos)
+        {
+            replace(pos, line, s1, s2);
+            pos += s2.size();
+        }
+        outfile << line << std::endl;
+    }
+
+    return 0;
 }
