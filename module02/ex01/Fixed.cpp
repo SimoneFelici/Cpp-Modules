91fa2899ec8cabc03c0468c80dcdb1a530803bc9
SimoneFelici <felicisimone9@gmail.com>
2025-09-24 17:43:03 +0200

    I love c++ (jkjkjkj)

--- module02/ex01/Fixed.cpp
+++ module02/ex01/Fixed.cpp
@@ -1,9 +1,20 @@
 #include "Fixed.hpp"
 #include <iostream>
+#include <cmath>
 
 Fixed::Fixed() : value(0)
 {
     std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int n) : value(n << fractional_num)
+{
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float f) : value(static_cast<int>(roundf(f * (1 << fractional_num))))
+{
+    std::cout << "Float constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed& other)
@@ -15,7 +26,7 @@ Fixed::Fixed(const Fixed& other)
 Fixed &Fixed::operator=(const Fixed& other)
 {
     std::cout << "Copy assignment operator called" << std::endl;
-    value = other.getRawBits();
+    value = other.value;
     return *this;
 }
 Fixed::~Fixed()
@@ -31,4 +42,20 @@ int Fixed::getRawBits(void) const
 void Fixed::setRawBits(int const raw)
 {
     value = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+    return static_cast<float>(value) / static_cast<float>(1 << fractional_num);
+}
+
+int Fixed::toInt(void) const
+{
+    return value >> fractional_num;
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fx)
+{
+    os << fx.toFloat();
+    return os;
 }
